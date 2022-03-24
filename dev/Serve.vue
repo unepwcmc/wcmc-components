5ca46cbaa84207ca512b1ebd121d219a21f69ec6
__joeburke <joseph_burke@outlook.com>
2022-03-24 13:40:27 +0000

    chore: comment explaining sort-icon slot

--- dev/Serve.vue
+++ dev/Serve.vue
@@ -6,7 +6,17 @@
     
     <filterable-table
       endpoint-download="/"
-    />
+    >
+    <!-- 
+      An example of how you might pass a named template for the sort icon into the FilterableTable:
+      
+      <template #sort-icon>
+        <span>
+          I'm the sorting icon now!
+        </span>
+      </template>
+    -->
+    </filterable-table>
   </div>
 </template>
 
