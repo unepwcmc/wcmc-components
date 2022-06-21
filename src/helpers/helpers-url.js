53071f3a29b6ed338ab339ed9c18bcd9fa35b40b
maxime <maxycle@hotmail.com>
2022-06-21 09:48:44 +0100

    fix: add toString method and z-index

--- src/helpers/helpers-url.js
+++ src/helpers/helpers-url.js
@@ -1,5 +1,5 @@
 export const isALink = (string) => {
-  if(string.includes('http')){
+  if(string.toString().includes('http')){
     return `<a href="${string}" title="Visit ${string}" target="_blank" />${string}</a>`
   } else {
     return string
