606466fb2af63c1ad3272afef4f2d1e8a9ae6aab
maxime <maxycle@hotmail.com>
2022-06-23 08:54:08 +0100

    fix: make link in the modal hyperlink, make hyperlinks blue

--- src/components/filterable-table/TableModal.vue
+++ src/components/filterable-table/TableModal.vue
@@ -74,7 +74,7 @@
                 v-text="item.value"
               />
 
-              <span v-else v-text="item.value" />
+              <span v-else v-html="printValue(item.value)" />
             </template>
           </div>
         </template>
