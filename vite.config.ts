46c16dfed124f3da7be25a4a72579b01bcb1d627
Yue-Long <yue-long.he@unep-wcmc.org>
2024-01-12 15:05:33 +0000

    feat: update packages

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@root': fileURLToPath(new URL('./', import.meta.url)),
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url))
     },
     preserveSymlinks: true
